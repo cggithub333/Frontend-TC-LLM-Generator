9844239450d870df2af1826707e6b773f5ca3cb5
Ha Hai Cuong <hhc9104@gmail.com>
2026-02-05 03:02:00 +0700

    feat: configure axios instance with interceptors

---
+++ src/lib/axios.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Add auth token if available
+    // const token = localStorage.getItem("token");
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Handle unauthorized
+      // window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
